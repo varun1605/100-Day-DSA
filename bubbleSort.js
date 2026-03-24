241e910474a2fe942f8e3b21d3ba62d267ce410f
Varun Mangrulkar <varunman.grulkar@gmail.com>
2026-03-24 02:53:14 +0000

    Bubble Sorting

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([0, 2, 8, 5, 7, 1]));
